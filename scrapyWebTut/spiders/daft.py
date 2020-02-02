ae44149b08ef44bd7d145a1c37e2e6d51040d58f
Lais Carvalho <lais.bsc@gmail.com>
2020-02-02 21:41:59 +0000

    first commit. from local to remote repo

---
+++ scrapyWebTut/spiders/daft.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.shell import inspect_response
+from scrapy_splash import SplashRequest
+
+
+class DaftSpider(scrapy.Spider):
+    name = 'daft'
+    allowed_domains = ['daft.ie']
+    start_urls = [
+        'https://www.daft.ie/dublin-city/commercial-property/retail-units/?ad_type=commercial&advanced=1&s%5Ba_id%5D%5B0%5D=3645&s%5Ba_id%5D%5B1%5D=2843&searchSource=commercial']
+
+    def start_requests(self):
+        yield SplashRequest(
+            self.start_urls[0],
+            callback=self.scrap_search_result_page,
+            args={
+                'wait': 0.5})
+
+    def scrap_search_result_page(self, response):
+        links = response.xpath("//div[@class='box']//span[@class='sr_counter']/following-sibling::a/@href").extract()
+        print(len(links))
+        for link in links:
+            yield SplashRequest(
+                'https://www.daft.ie' + link,  # It is a relative url so we will concat them
+                callback=self.parse,  # Set the callback to parse as this will parse the specific commerial let
+                args={
+                    'wait': 0.5}) # give splash time to render can be lowered
+
+    def parse(self, response):
+        print(response.url)
+        rent_price = response.xpath('//div[@id="smi-price-string"]//text()').get()
+        location = response.xpath('//div[@id="address_box"]//h1/text()').get()
+        size = response.xpath('//div[@id="address_box"]//span[contains(text(),"feet")]//text()').get()
+        how_many_times_views = response.xpath(
+            '//div[@class="description_extras"]//h3[contains(text(),"Property Views:")]/following-sibling::text()[1]').get()
+        yield rent_price
+        yield location
+        yield size
+        yield how_many_times_views
